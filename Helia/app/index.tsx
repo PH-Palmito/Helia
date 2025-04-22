ba00ddf77cd5bf181883ea790f0a6db3c3acb468
PH-Palmito <phpalmito@gmail.com>
2025-04-22 18:10:07 -0300

    .

--- Helia/app/index.tsx
+++ Helia/app/index.tsx
@@ -1,6 +1,6 @@
-import { Text, View, StyleSheet,} from "react-native";
+import { Text, View, StyleSheet, TextInput,} from "react-native";
 import {StatusBar} from "expo-status-bar";
-import { Bookmark, ChatsTeardrop } from "phosphor-react-native";
+import { Bookmark, ChatsTeardrop, MagnifyingGlass, SlidersHorizontal, TextIndent } from "phosphor-react-native";
 import { BellRinging } from "phosphor-react-native";
 
 export default function Index() {
@@ -10,15 +10,26 @@ export default function Index() {
 
     <View style={styles.header}>
       <View style={styles.headerLeft}><ChatsTeardrop  size={30}
-      color="1ab65c" weight="duotone"
+      color="#1ab65c" weight="duotone"
       />
-        <text style={styles.headerLeftText}>Helia</text>
+        <Text style={styles.headerLeftText}>Helia</Text>
       </View>
      <View style={styles.headerRight}>
-     <BellRinging  size={30} color="f4f4f4" weight="duotone"/>
-     <Bookmark size={30} color="f4f4f4" weight="duotone"/>
+     <BellRinging  size={30} color="#f4f4f4" weight="duotone"/>
+     <Bookmark size={30} color="#f4f4f4" weight="duotone"/>
      </View>
     </View>
+
+    <Text style={styles.userName}>Ola, Pedro H</Text>
+    <View style={styles.inputcontainer}>
+      <MagnifyingGlass size={30} color="#757575"
+      weight="duotone"/>
+      <TextInput
+      style={styles.input} placeholder="Busque uma casa aqui" placeholderTextColor="#757575"/>
+      <SlidersHorizontal size={30} color="#1ab65c"
+      weight="duotone"/>
+    </View>
+    
   </View>
 
 
@@ -28,7 +39,7 @@ export default function Index() {
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#181a28",
     paddingHorizontal: 20,
   },
   header:{
@@ -36,7 +47,42 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  headerLeft:{},
-  headerLeftText:{},
-  headerRight:{},
+    headerLeft:{
+    flexDirection: "row",
+    alignItems: "center" ,
+    gap: 10,
+
+  },
+  headerRight:{
+    flexDirection: "row",
+    alignItems: "center" ,
+    gap: 10,
+  },
+  headerLeftText:{
+    color: "#ffff",
+    fontSize: 24,
+    fontWeight: "800",
+  },
+  userName:{
+ paddingTop: 30,
+ color: "#f4f4f4",
+ fontSize: 25,
+ fontWeight: "800",
+  },
+  inputcontainer:{
+    width	: "100%",
+    height:56,
+    backgroundColor: "#1f222a",
+    borderRadius: 12,
+    flexDirection:"row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    gap: 10,
+    marginTop: 20,
+
+  },
+  input:{
+    flex: 1,
+    color: "#f4f4f4",
+  },
 });
