e67eef5ba4c2ea8f3bfb6af4ea9876a211c3e158
PH-Palmito <phpalmito@gmail.com>
2025-04-29 23:18:17 -0300

    Update details.tsx

--- Helia/app/stacks/details.tsx
+++ Helia/app/stacks/details.tsx
@@ -1,5 +1,5 @@
-import { ArrowLeft, BookBookmark, DotsThreeCircle, FileX } from "phosphor-react-native";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { ArrowLeft, BookBookmark, DotsThreeCircle, FileX, MapPin } from "phosphor-react-native";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Details() {
     return (
@@ -17,6 +17,33 @@ export default function Details() {
         </View>
 
         </View>
+        <Text style={styles.infoNameText}>Hotel Nova Vista</Text>
+
+         <View style={styles.contentAddress}>
+            <MapPin size={32} color="#1ab65c" />
+            <Text style={styles.contentAddressText}>Avenida Paralela, 123</Text>
+         </View>
+         <View style={styles.separator}/>
+         <View style={styles.containerGalleryPhotos}>
+            <Text style={styles.containerGalleryPhotosText}>Galeria de Fotos</Text>
+            <Text style={styles.containerGalleryPhotosSeeAll}>Ver Todas</Text>
+
+         </View>
+         <ScrollView horizontal style={styles.contentPhotoContainer}>
+            <Image style={styles.contentPhotoImage} source={require("../assets/images/hotel.jpg")}/>
+            <Image style={styles.contentPhotoImage} source={require("../assets/images/hotel.jpg")}/>
+            <Image style={styles.contentPhotoImage} source={require("../assets/images/hotel.jpg")}/>
+            <Image style={styles.contentPhotoImage} source={require("../assets/images/hotel.jpg")}/>
+         </ScrollView>
+         <View style={styles.footer}>
+            <View style={styles.footercontainerText}>
+                    <Text style={styles.footercontainerTextMoney}>R$200</Text>
+                    <Text style={styles.footercontainerTextMonth}>/Diaria</Text>
+            </View>
+              <TouchableOpacity style={styles.button} >
+                            <Text style={styles.buttonText}>Reservar</Text>
+             </TouchableOpacity>
+         </View>
     </View>
     );
 }
@@ -35,8 +62,8 @@ export const styles = StyleSheet.create({
         position: "absolute",
     },
     headerinfoButtons:{
-        marginTop: 40,
-        paddingHorizontal: 40,
+        marginTop: 80,
+        paddingHorizontal: 30,
         flexDirection: "row",
         justifyContent: "space-between",
 
@@ -46,4 +73,104 @@ export const styles = StyleSheet.create({
         gap: 10,
 
     },
+    infoNameText:{
+        color: "#f4f4f4",
+        fontSize:36,
+        fontWeight: "bold",
+        paddingHorizontal: 30,
+       paddingTop: 20,
+
+    },
+    contentAddress:{
+   flexDirection: "row",
+   alignItems: "center",
+   gap: 10,
+   paddingHorizontal: 30,
+   paddingTop: 15,
+},
+    contentAddressText:{
+
+        color: "#f4f4f4",
+        fontSize: 12,
+},
+    separator:{
+    height: 1,
+    backgroundColor: "#757575",
+    marginHorizontal: 30,
+    marginTop: 15,
+},
+    containerGalleryPhotos:{
+    flexDirection: "row",
+    paddingHorizontal: 30,
+    marginTop: 20,
+    justifyContent: "space-between",
+},
+    containerGalleryPhotosText:{
+    color: "#f4f4f4",
+    fontSize: 18,
+    fontWeight: "bold",
+},
+    containerGalleryPhotosSeeAll:{
+    fontSize: 12,
+    color: "#1ab65c",
+    fontWeight: "400",
+},
+contentPhotoContainer:{
+
+    paddingHorizontal:30,
+    paddingTop:15,
+
+},
+contentPhotoImage:{
+    width: 100,
+    height:100,
+    borderRadius: 12,
+    marginRight:10,
+},
+footer:{
+    width:"100%",
+    borderWidth:1,
+    height:80,
+    borderRightColor: "#757575",
+    borderLeftColor:"#757575",
+    borderTopColor:"#757575",
+
+    borderTopLeftRadius: 26,
+    borderTopRightRadius: 26,
+    flex:1,
+    flexDirection:"row",
+    paddingHorizontal:10,
+    gap:10,
+
+},
+footercontainerText:{
+    flexDirection:"row",
+    alignItems:"center",
+    gap:10,
+
+},
+footercontainerTextMoney:{
+fontSize:30,
+fontWeight:"bold",
+color:"#1ab65c",
+},
+footercontainerTextMonth:{
+    fontSize:12,
+    fontWeight:"400",
+    color:"#f4f4f4",
+},
+button:{
+    backgroundColor: "#1ab55c",
+    flex: 1,
+    height: 56,
+    borderRadius: 32,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+},
+buttonText:{
+    color: "#f4f4f4",
+    fontSize: 16,
+    fontWeight: "800",
+},
 });
